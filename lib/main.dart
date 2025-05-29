46c98b1733ebc93e2ca4d7c8cd5ee51695cfdc28
dtech-innovation <admin@dtechinovation.com>
2025-05-29 11:56:15 +0700

    Change to ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,9 @@ class MyApp extends StatelessWidget {
         body:Center(
             child: Column(
               children: [
-                MaterialButton(
+                ElevatedButton(
                   onPressed: () {},
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                          colors: [Colors.blue, Colors.red]),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Container(
-                      constraints:
-                          const BoxConstraints(minWidth: 88.0, minHeight: 36.0),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'Gradient Button',
-                        style: TextStyle(color: Colors.white, fontSize: 17),
-                      ),
-                    ),
-                  ),
+                  child: const Text('Press Me'),
                 )
               ],
             ),
