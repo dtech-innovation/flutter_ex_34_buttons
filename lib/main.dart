af87738e99672df6abaefe6c32f209d608f40ae9
dtech-innovation <admin@dtechinovation.com>
2025-05-29 10:48:25 +0700

    change to Simple MaterialButton

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Buttons In Flutter'),
+        appBar: AppBar(title: const Text('Buttons In Flutter')),
+        body: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: const Text('Press Me'),
+              ),
+            ],
           ),
-          body: Center(
-            child: Column(
-              children: [
-                MaterialButton(
-                  onPressed: () {
-                    // action to perform when button is pressed
-                  },
-                  color: Colors.blue,
-                  child: const Text('Press Me',
-                      style: TextStyle(color: Colors.white)),
-                )
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
